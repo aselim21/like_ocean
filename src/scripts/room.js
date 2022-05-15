bae45aa5044091913e582ba92e0b684a468e79f7
Achelia Selim <73719319+aselim21@users.noreply.github.com>
2022-05-15 10:01:34 +0300

    go

--- src/scripts/room.js
+++ src/scripts/room.js
@@ -194,7 +194,7 @@ async function getParticipantsInfo() {
         method: 'GET'
     });
     console.log(response);
-    console.log(response.json());//test
+    console.log(response.json());//test//tests
     return response;
 }
 async function updateParticipantsInfo(data) {
